8f9021a655c22d8505c13012baa3fefc8f73fa63
Nenad <nenad@Nenads-MacBook-Pro-4.local>
2019-02-15 11:40:18 +0100

    set image size to 20

--- Sources/FlagPhoneNumberTextField.swift
+++ Sources/FlagPhoneNumberTextField.swift
@@ -128,7 +128,7 @@ open class FPNTextField: UITextField, FPNCountryPickerDelegate, FPNDelegate {
 	}
 
 	private func setupFlagButton() {
-        
+        flagButton.frame.size = CGSize(width: 20, height: 20)
 		flagButton.contentHorizontalAlignment = .left
 		flagButton.contentVerticalAlignment = .fill
 		flagButton.imageView?.contentMode = .scaleAspectFit
